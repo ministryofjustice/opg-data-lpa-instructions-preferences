bb64a767a82b69e5cae5d68185abfc02a2210dea
James Warren <james.warren@digital.justice.gov.uk>
2023-02-13 16:46:17 +0000

    UML-2800 add infrastructure for iap

--- lambdas/image_request_handler/app/handler.py
+++ lambdas/image_request_handler/app/handler.py
@@ -165,12 +165,21 @@ class ImageRequestHandler:
 
 def lambda_handler(event, context):
     environment = os.getenv("ENVIRONMENT")
-
+    print(event['pathParameters'])
     s3_image_request_handler = ImageRequestHandler(
-        uid=event['uid'],
-        bucket=f'ual-iap-{environment}',
-        sqs_queue=f'ual-iap-image-request-{environment}'
+        uid=event['pathParameters']['uid'],
+        bucket=f'lpa-iap-{environment}',
+        sqs_queue=f'{environment}-lpa-iap-requests'
     )
     message = s3_image_request_handler.process_request()
 
-    return message
+    status_code = 500 if message['status'] == 'COLLECTION_ERROR' else 200
+
+    response = {
+        "isBase64Encoded": False,
+        "statusCode": status_code,
+        "headers": {},
+        "body": message
+    }
+
+    return json.dumps(response)
