512409b86a2dd5a1ff93059cfd8ffb5eeaab5e36
James Warren <james.warren@digital.justice.gov.uk>
2023-03-01 16:45:14 +0000

    UML-2769 infra for iap

--- terraform/environment/lambda.tf
+++ terraform/environment/lambda.tf
@@ -11,6 +11,7 @@ module "request_handler_lamdba" {
   working_directory = "/var/task"
   environment_variables = {
     ENVIRONMENT = local.environment
+    VERSION     = "v1"
   }
   image_uri         = "${data.aws_ecr_repository.lpa_iap_request_handler.repository_url}:${var.image_tag}"
   ecr_arn           = data.aws_ecr_repository.lpa_iap_request_handler.arn
@@ -20,6 +21,35 @@ module "request_handler_lamdba" {
   aws_subnet_ids    = []
   logs_kms_key      = data.aws_kms_key.lpa_iap_logs
   retention_in_days = 365
+}
+
+data "aws_ecr_repository" "lpa_iap_processor" {
+  provider = aws.management
+  name     = "integrations/lpa-iap-scan-processor-lambda"
+}
+
+module "processor_lamdba" {
+  source            = "./modules/lambda"
+  lambda_name       = "lpa-iap-processor-${local.environment}"
+  description       = "Function to process scanned documents and extract instructions and preferences"
+  working_directory = "/var/task"
+  environment_variables = {
+    ENVIRONMENT        = local.environment
+    SIRIUS_URL         = var.use_mock_sirius == "1" ? "http://mock-sirius.lpa-iap-${local.environment}.ecs" : "http://api.${local.account.target_environment}.ecs"
+    SESSION_DATA       = local.session_data
+    TARGET_ENVIRONMENT = local.account.target_environment
+    SECRET_PREFIX      = local.account.secret_prefix
+    SIRIUS_URL_PART    = "api/public/v1"
+  }
+  image_uri          = "${data.aws_ecr_repository.lpa_iap_processor.repository_url}:${var.image_tag}"
+  ecr_arn            = data.aws_ecr_repository.lpa_iap_request_handler.arn
+  account            = local.account
+  environment        = local.environment
+  rest_api           = aws_api_gateway_rest_api.lpa_iap
+  aws_subnet_ids     = data.aws_subnets.private.ids
+  security_group_ids = [data.aws_security_group.lambda_api_ingress.id]
+  logs_kms_key       = data.aws_kms_key.lpa_iap_logs
+  retention_in_days  = 365
 }
 
 # Needed for next lambda
