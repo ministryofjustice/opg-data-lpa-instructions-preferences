8cb6a6f8803f37b3e6a3dba41ceb0e93430c3f05
James Warren <james.warren@digital.justice.gov.uk>
2023-03-01 16:45:14 +0000

    UML-2769 infra for iap

---
+++ terraform/environment/mock_sirius.tf
@@ -0,0 +1,15 @@
+module "mock_sirius" {
+  count               = contains(keys(var.accounts), local.environment) ? 0 : 1
+  source              = "./modules/mock_sirius"
+  vpc_id              = local.account.vpc_id
+  environment         = local.environment
+  use_mock_sirius     = var.use_mock_sirius
+  subnets             = data.aws_subnet_ids.private.ids
+  image_tag           = var.image_tag
+  target_environment  = local.account.target_environment
+  s3_vpc_endpoint_ids = local.account.s3_vpc_endpoint_ids
+  providers = {
+    aws            = aws,
+    aws.management = aws.management
+  }
+}
