b21a1ba577db4b812057c122ed1a768d86e2d16b
James Warren <james.warren@digital.justice.gov.uk>
2023-03-08 13:09:27 +0000

    fix requirements and use pip form-tools

--- lambdas/image_processor/tests/handler_test.py
+++ lambdas/image_processor/tests/handler_test.py
@@ -17,7 +17,8 @@ event = {'Records': [{'body': '{\"uid\": \"700000000005\"}'}]}
 def setup_environment_variables():
     os.environ["SIRIUS_URL"] = "http://sirius-test"
     os.environ["ENVIRONMENT"] = "testing"
-    os.environ["TARGET_ENVIRONMENT"] = "target_testing"
+    os.environ["TARGET_ENVIRONMENT"] = "target-testing"
+    os.environ["SESSION_DATA"] = 'ops@email.com'
 
 
 @pytest.fixture
@@ -30,7 +31,7 @@ def test_init(image_processor):
     assert image_processor.sirius_url == os.getenv('SIRIUS_URL')
     assert image_processor.target_environment == os.getenv('TARGET_ENVIRONMENT')
     assert image_processor.event == event
-    assert image_processor.sirius_bucket == f"opg-backoffice-datastore-{os.getenv('ENVIRONMENT')}"
+    assert image_processor.sirius_bucket == f"opg-backoffice-datastore-{os.getenv('TARGET_ENVIRONMENT')}"
     assert image_processor.iap_bucket == f"lpa-iap-{os.getenv('ENVIRONMENT')}"
     assert image_processor.uid == None
 
@@ -68,33 +69,45 @@ def mock_get():
         yield mock_get
 
 
-def test_make_request_to_sirius_successful(mock_get):
+def test_make_request_to_sirius_successful(mock_get, monkeypatch):
     # Setup the mock response
     mock_response = MagicMock()
     mock_response.text = '{"key": "value"}'
     mock_get.return_value = mock_response
 
-    # Call the method with a valid UID
     image_processor = ImageProcessor(event)
+    mock_sirius_headers = MagicMock()
+    mock_sirius_headers.return_value = {
+        "Content-Type": 'application/json',
+        "Authorization": "Bearer 1234"
+    }
+    # Call the method with a valid UID
+    monkeypatch.setattr(image_processor, "build_sirius_headers", mock_sirius_headers)
     response_dict = image_processor.make_request_to_sirius(test_uid)
 
     # Verify the response
     assert response_dict == {"key": "value"}
 
 
-def test_make_request_to_sirius_exception(mock_get):
+def test_make_request_to_sirius_exception(mock_get, monkeypatch):
     # Setup the mock request exception
     mock_get.side_effect = requests.exceptions.RequestException("Test Exception")
 
-    # Call the method with a valid UID
     image_processor = ImageProcessor(event)
+    mock_sirius_headers = MagicMock()
+    mock_sirius_headers.return_value = {
+        "Content-Type": 'application/json',
+        "Authorization": "Bearer 1234"
+    }
+    monkeypatch.setattr(image_processor, "build_sirius_headers", mock_sirius_headers)
+    # Call the method with a valid UID
     response_dict = image_processor.make_request_to_sirius(test_uid)
 
     # Verify the response
     assert response_dict == {"error": "error getting response from Sirius"}
 
 
-def test_make_request_to_sirius_decode_exception(mock_get):
+def test_make_request_to_sirius_decode_exception(mock_get, monkeypatch):
     # Setup the mock response
     mock_response = MagicMock()
     mock_response.text = 'Invalid JSON'
@@ -102,6 +115,14 @@ def test_make_request_to_sirius_decode_exception(mock_get):
 
     # Call the method with a valid UID
     image_processor = ImageProcessor(event)
+
+    mock_sirius_headers = MagicMock()
+    mock_sirius_headers.return_value = {
+        "Content-Type": 'application/json',
+        "Authorization": "Bearer 1234"
+    }
+    monkeypatch.setattr(image_processor, "build_sirius_headers", mock_sirius_headers)
+
     response_dict = image_processor.make_request_to_sirius(test_uid)
 
     # Verify the response
@@ -145,21 +166,22 @@ def test_download_scanned_images(image_processor, monkeypatch):
 
     # Check that the expected S3 files were downloaded
     assert len(mock_download_file.mock_calls) == 3
-    mock_download_file.assert_any_call("my_bucket", "my_scan.pdf", "extraction/my_scan.pdf")
-    mock_download_file.assert_any_call("my_bucket", "my_continuation_sheet1.pdf", "extraction/my_continuation_sheet1.pdf")
-    mock_download_file.assert_any_call("my_bucket", "my_continuation_sheet2.pdf", "extraction/my_continuation_sheet2.pdf")
+    mock_download_file.assert_any_call("my_bucket", "my_scan.pdf", "/tmp/output/my_scan.pdf")
+    mock_download_file.assert_any_call("my_bucket", "my_continuation_sheet1.pdf", "/tmp/output/my_continuation_sheet1.pdf")
+    mock_download_file.assert_any_call("my_bucket", "my_continuation_sheet2.pdf", "/tmp/output/my_continuation_sheet2.pdf")
 
     # Check that the function returned the expected file paths
     expected_result = {
-        "scan": "extraction/my_scan.pdf",
-        "continuation_1": "extraction/my_continuation_sheet1.pdf",
-        "continuation_2": "extraction/my_continuation_sheet2.pdf",
+        "scan": "/tmp/output/my_scan.pdf",
+        "continuation_1": "/tmp/output/my_continuation_sheet1.pdf",
+        "continuation_2": "/tmp/output/my_continuation_sheet2.pdf",
     }
     assert result == expected_result
 
 
 def test_extract_instructions_and_preferences(image_processor, monkeypatch):
-    extraction_folder_path = "extraction"
+    temp_output = "/tmp/output"
+    extraction_folder_path = 'extraction'
     image_locations = {"scan": f"{extraction_folder_path}/test_image.jpg"}
 
     # Create a mock object for FormOperator
@@ -171,8 +193,8 @@ def test_extract_instructions_and_preferences(image_processor, monkeypatch):
 
     list_files_mock = MagicMock()
     list_files_mock.return_value = [
-            f"{extraction_folder_path}/pass/1234/field_name=instructions/image_instructions.jpg",
-            f"{extraction_folder_path}/pass/1234/field_name=preferences/image_preferences.jpg",
+            f"{temp_output}/pass/1234/field_name=instructions/image_instructions.jpg",
+            f"{temp_output}/pass/1234/field_name=preferences/image_preferences.jpg",
         ]
 
     monkeypatch.setattr(image_processor, "list_files", list_files_mock)
@@ -186,19 +208,19 @@ def test_extract_instructions_and_preferences(image_processor, monkeypatch):
 
     # Assert the output is as expected
     assert result == {
-        "instructions": f"{extraction_folder_path}/pass/1234/field_name=instructions/image_instructions.jpg",
-        "preferences": f"{extraction_folder_path}/pass/1234/field_name=preferences/image_preferences.jpg",
+        "instructions": f"{temp_output}/pass/1234/field_name=instructions/image_instructions.jpg",
+        "preferences": f"{temp_output}/pass/1234/field_name=preferences/image_preferences.jpg",
     }
 
     # Assert that the mocked functions were called as expected
     mock_form_operator.run_full_pipeline.assert_called_once_with(
         form_path=image_locations["scan"],
-        pass_dir=f"{extraction_folder_path}/pass/1234",
-        fail_dir=f"{extraction_folder_path}/fail/1234",
+        pass_dir=f"{temp_output}/pass/1234",
+        fail_dir=f"{temp_output}/fail/1234",
         form_meta_directory=f"{extraction_folder_path}/metadata",
     )
     list_files_mock.assert_called_once_with(
-        f"{extraction_folder_path}/pass/1234", ".jpg"
+        f"{temp_output}/pass/1234", ".jpg"
     )
 
 
--- terraform/environment/modules/mock_sirius/terraform.tf
+++ terraform/environment/modules/mock_sirius/terraform.tf
@@ -1,13 +1,3 @@
 provider "aws" {
   alias = "management"
 }
-
-#terraform {
-#  required_version = ">= 1.0.0"
-#  required_providers {
-#    aws = {
-#      source                = "hashicorp/aws"
-#      configuration_aliases = [aws.management]
-#    }
-#  }
-#}
