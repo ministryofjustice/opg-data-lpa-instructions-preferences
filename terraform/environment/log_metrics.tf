b443042ad762192beda8846effbe9b45b291052a
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-06-29 11:38:26 +0100

    UML-2925 Fix linting

--- terraform/environment/log_metrics.tf
+++ terraform/environment/log_metrics.tf
@@ -1,25 +1,25 @@
 resource "aws_cloudwatch_log_metric_filter" "pdf_sizes_bytes" {
   name           = "PDF Sizes (Bytes)"
-  pattern        = { $.pdf_size == "*" }
+  pattern        = "$.pdf_size == \"*\""
   log_group_name = aws_cloudwatch_log_group.lambda.name
 
   metric_transformation {
-    name          = "PDFSize"
-    namespace     = "IaPPDFStatistics"
-    value         = $.pdf_size
-    unit          = "bytes"
+    name      = "PDFSize"
+    namespace = "IaPPDFStatistics"
+    value     = "$.pdf_size"
+    unit      = "bytes"
   }
 }
 
 
 resource "aws_cloudwatch_log_metric_filter" "pdf_length_pages" {
   name           = "PDF Length (Pages)"
-  pattern        = { $.pdf_length == "*" }
+  pattern        = "$.pdf_length == \"*\""
   log_group_name = aws_cloudwatch_log_group.lambda.name
 
   metric_transformation {
-    name          = "PDFLength"
-    namespace     = "IaPPDFStatistics"
-    value         = $.pdf_length
+    name      = "PDFLength"
+    namespace = "IaPPDFStatistics"
+    value     = "$.pdf_length"
   }
 }
