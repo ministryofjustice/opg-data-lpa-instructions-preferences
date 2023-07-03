3c5cf672d5bf3334327ae07f7344bb0aefce262a
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-07-03 17:56:34 +0100

    Terraform linting

--- terraform/environment/api_rest.tf
+++ terraform/environment/api_rest.tf
@@ -1,4 +1,4 @@
-  resource "aws_api_gateway_rest_api" "lpa_iap" {
+resource "aws_api_gateway_rest_api" "lpa_iap" {
   name        = "lpa-instructions-preferences-${local.environment}"
   description = "API Gateway for LPA instructions and preferences - ${local.environment}"
   body        = templatefile(local.openapi_spec, local.api_template_vars)
