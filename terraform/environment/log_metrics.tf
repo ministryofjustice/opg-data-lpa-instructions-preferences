92d201dff0e72f3b01f305d2fa6d6c957270db55
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-12-27 13:53:38 +0000

    UML-2763: One alarm per environment

--- terraform/environment/log_metrics.tf
+++ terraform/environment/log_metrics.tf
@@ -1,3 +1,7 @@
+data "aws_sns_topic" "cloudwatch_to_pagerduty" {
+  name = "CloudWatch-to-PagerDuty-${local.account.name}-InstructionsAndPreferences"
+}
+
 resource "aws_cloudwatch_log_metric_filter" "pdf_sizes_bytes" {
   name           = "PDF Sizes (Bytes)"
   pattern        = "{ $.pdfSize = \"*\" }"
@@ -41,4 +45,18 @@ resource "aws_cloudwatch_log_metric_filter" "image_processor_error_count" {
     namespace = "IaP/Stats"
     value     = "1"
   }
+}
+
+resource "aws_cloudwatch_metric_alarm" "iap_error_count" {
+  alarm_name          = "${local.environment}-IAP-Error-Count"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.image_processor_error_count.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.image_processor_error_count.metric_transformation[0].namespace
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "3"
+  alarm_description   = "This alarm is triggered when there are more than 3 IAP errors in a 5 minute period."
+  alarm_actions       = ["${data.aws_sns_topic.cloudwatch_to_pagerduty.arn}"]
+  treat_missing_data  = "notBreaching"
 }
