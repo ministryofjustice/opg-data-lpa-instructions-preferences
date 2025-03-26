ea3d90cc202ff90fbefe37547576238c62fd7bcc
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2025-03-26 17:08:14 +0000

    2cnd test and added files

--- integration/config.py
+++ integration/config.py
@@ -324,4 +324,25 @@ templates = {
             },
         },
     },
+    "LPC-hi-res": {
+        "lpa_uid": "700000000101",
+        "expected_collection_started_response": {
+            "uId": "700000000101",
+            "status": "COLLECTION_NOT_STARTED",
+            "signedUrls": {},
+        },
+        "expected_collection_in_progress_response": {
+            "uId": "700000000101",
+            "status": "COLLECTION_IN_PROGRESS",
+            "signedUrls": {},
+        },
+        "expected_collection_completed_response": {
+            "uId": "700000000101",
+            "status": "COLLECTION_COMPLETE",
+            "signedUrls": {
+                "iap-700000000101-instructions": "",
+                "iap-700000000101-preferences": "",
+            },
+        },
+    },
 }
