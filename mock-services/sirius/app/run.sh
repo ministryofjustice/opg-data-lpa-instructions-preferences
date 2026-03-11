bb968ec0c9c634458e4213e00f4fb6edb64b33f2
Matt <Matt.Machell@publicguardian.gov.uk>
2026-03-11 16:29:01 +0000

    Try a lighter custom build of prism

--- mock-services/sirius/app/run.sh
+++ mock-services/sirius/app/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-node /usr/src/prism/packages/cli/dist/index.js mock -h 0.0.0.0 -p 4010 sirius.yml
+node ./node_modules/@stoplight/prism-cli/dist/index.js mock -h 0.0.0.0 -p 4010 sirius.yml
